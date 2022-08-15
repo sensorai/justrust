25e7f1387d8fe2f4d64d8a5884caee7457068db4
Austin Chen <austin.yhc@gmail.com>
2022-08-15 10:47:55 +0800

    :medal_military: More about printing and string

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ Rust is a language that is quite new, but already very popular. It's popular bec
 # Who am I?
 
 I wrote Just Rust while thinking of how to make it easy for companies here to start using it.
+
+
+## String
+
+Rust has two main types of strings: `String` and `&str`. What is the difference?
+
+* `&str` is a simple string. When you write `let my_variable = "Hello, world!"`, you create a `&str`. A `&str` is very fast.
+* `String` is a more complicated string. It is a bit slower, but it has more functions. A `String` is a pointer, with data on the heap.
+
+Also note that `&str` has the `&` in front of it because you need a reference to use a `str`. That's because of the reason we saw above: the stack needs to know the size. So we give it a `&` that it knows the size of, and then it is happy. Also, because you use a `&` to interact with a `str`, you don't own it. But a `String` is an owned type. We will soon learn why that is important to know.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{city1} is in {country} and {city2} is also in {country}
+             but {city3} is not in {country}",
+             city1 = "Taipei",
+             city2 = "Taichung",
+             city3 = "ShenZhen",
+             country = "Taiwan");
+
+    let letter = "a";
+    println!("{:b^11}", letter);
+
+    let title = "AUSTIN CAN HELP";
+    println!("{:-^32}", title);
+    println!("{: <16}{: >16}", '|', '|');
+    println!("{:-<16}{:->16}", "FRAN", "FRAN");
+
+    println!("A String is always {:?} bytes. It is Sized.", std::mem::size_of::<String>());
+    println!("An i8 is always {:?} bytes. It is Sized.", std::mem::size_of::<i8>());
+    println!("An u8 is always {:?} bytes. It is Sized.", std::mem::size_of::<u8>());
+    println!("But a &str? It can be anything.'陳昱宏' is {:?} bytes. It is not Sized.", std::mem::size_of_val("陳昱宏"));
+
+    let mut my_name: String = "Austin".to_string();
+    println!("{}", my_name);
+    my_name.push_str(" Chen");
+    println!("{}", my_name);
+    let last_name = &my_name[6..];
+    println!("{}", last_name);
 }
