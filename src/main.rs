9961a915e201c27a916bf0dcb6ac5f9a4e7cfacf
Austin Chen <austin.yhc@gmail.com>
2022-09-27 15:45:42 +0800

    :zap: Add source codes

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,17 @@
-fn main() {
-    println!("{city1} is in {country} and {city2} is also in {country}
-             but {city3} is not in {country}",
-             city1 = "Taipei",
-             city2 = "Taichung",
-             city3 = "ShenZhen",
-             country = "Taiwan");
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
 
-    let letter = "a";
-    println!("{:b^11}", letter);
+use cortex_m_rt::entry;
+use panic_halt as _;
+use microbit as _;
 
-    let title = "AUSTIN CAN HELP";
-    println!("{:-^32}", title);
-    println!("{: <16}{: >16}", '|', '|');
-    println!("{:-<16}{:->16}", "FRAN", "FRAN");
+#[entry]
+fn main() -> ! {
+    let _y;
+    let x = 42;
+    _y = x;
 
-    println!("A String is always {:?} bytes. It is Sized.", std::mem::size_of::<String>());
-    println!("An i8 is always {:?} bytes. It is Sized.", std::mem::size_of::<i8>());
-    println!("An u8 is always {:?} bytes. It is Sized.", std::mem::size_of::<u8>());
-    println!("But a &str? It can be anything.'陳昱宏' is {:?} bytes. It is not Sized.", std::mem::size_of_val("陳昱宏"));
-
-    let mut my_name: String = "Austin".to_string();
-    println!("{}", my_name);
-    my_name.push_str(" Chen");
-    println!("{}", my_name);
-    let last_name = &my_name[6..];
-    println!("{}", last_name);
+    // infinite loop; just so we don't leave this stack frame
+    loop {}
 }
