da716b482af189b3a4d953cd0ea8d7089df31ef2
Austin Chen <austin.yhc@gmail.com>
2022-09-27 18:22:29 +0800

    :bookmark: Successfully add bsp.rs to the project

--- src/10-punch-o-meter/src/main.rs
+++ src/10-punch-o-meter/src/main.rs
@@ -1,15 +1,28 @@
-#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
 use cortex_m_rt::entry;
 use rtt_target::rtt_init_print;
+//use nrf52832_hal::Timer;
 use panic_rtt_target as _;
+
+mod bsp;
+use crate::bsp::Board;
+use nb::block;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    let board = microbit::Board::take().unwrap();
-
-    loop {}
+    let mut board = Board::take().unwrap();
+    //let mut timer = Timer::new(board.TIMER0);
+    let mut led_is_on = false;
+    loop {
+        if led_is_on {
+            board.leds.led_2.disable();
+        } else {
+            board.leds.led_2.enable();
+        }
+        //block!(timer.wait()).unwrap();
+        led_is_on = !led_is_on;
+    }
 }
